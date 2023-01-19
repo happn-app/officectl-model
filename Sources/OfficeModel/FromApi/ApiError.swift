46496cc707d4ada5f8c7b142f4b13a4d2fea1494
Frizlab <fload@me.com>
2023-01-19 15:01:43 +0100

    Only leave the error message in the ApiError struct

--- Sources/OfficeModel/FromApi/ApiError.swift
+++ Sources/OfficeModel/FromApi/ApiError.swift
@@ -4,14 +4,10 @@ import Foundation
 
 public struct ApiError : Error, Codable, Sendable {
 	
-	public var code: Int
-	public var domain: String
 	public var message: String
 	
-	public init(code c: Int, domain d: String, message m: String) {
-		code = c
-		domain = d
-		message = m
+	public init(message: String) {
+		self.message = message
 	}
 	
 }
