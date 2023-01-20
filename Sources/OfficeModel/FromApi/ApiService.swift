d4b6b9d619be09d8f060463b362511660d42f671
Frizlab <fload@me.com>
2023-01-20 20:08:23 +0100

    Add more info in the service object

--- Sources/OfficeModel/FromApi/ApiService.swift
+++ Sources/OfficeModel/FromApi/ApiService.swift
@@ -9,10 +9,23 @@ public struct ApiService : Codable, Sendable {
 	public var serviceID: String
 	public var serviceFullName: String
 	
-	public init(providerID: String, serviceID: String, serviceFullName: String) {
+	public var isAuthService: Bool
+	public var isUserService: Bool
+	public var isGroupOfUsersService: Bool
+	
+	public init(
+		providerID: String,
+		serviceID: String, serviceFullName: String,
+		isAuthService: Bool, isUserService: Bool, isGroupOfUsersService: Bool
+	) {
 		self.providerID = providerID
+		
 		self.serviceID = serviceID
 		self.serviceFullName = serviceFullName
+		
+		self.isAuthService = isAuthService
+		self.isUserService = isUserService
+		self.isGroupOfUsersService = isGroupOfUsersService
 	}
 	
 }
