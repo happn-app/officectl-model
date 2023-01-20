8dc2b1c7cc8530329d8368ad637bd9b9aa5cd533
Frizlab <fload@me.com>
2023-01-20 15:51:02 +0100

    Fix ApiUsers definition

--- Sources/OfficeModel/FromApi/User/ApiUsers.swift
+++ Sources/OfficeModel/FromApi/User/ApiUsers.swift
@@ -8,6 +8,6 @@ import Foundation
  This is strangely _not_ directly an array of ``ApiUser``.
  The reason for this is we want to be able to convey the information that a service failed to return its list of users.
  
- We do not however want to return the list of all the merged users as this not really useful and would take a lot of space.
- So instead of an array of ``ApiMergedUser``, the `MergedResults` type for `ApiUsers` is `None`. */
-public typealias ApiUsers = ApiMultiServicesResults<None, [ApiUser]>
+ We do not however want to return the list of all the users per service as this is not very useful.
+ So instead of an array of ``ApiDirectoryUser``, the `Results` type for `ApiUsers` is `None`. */
+public typealias ApiUsers = ApiMultiServicesResults<[ApiUser], None>
