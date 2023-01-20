980d17022b245b14541df919a2652c6056250518
Frizlab <fload@me.com>
2023-01-20 15:55:48 +0100

    Add public inits for ApiMultiServicesID

--- Sources/OfficeModel/FromApi/ApiMultiServicesID.swift
+++ Sources/OfficeModel/FromApi/ApiMultiServicesID.swift
@@ -8,6 +8,15 @@ public struct ApiMultiServicesID : Sendable, Codable {
 	
 	public var ids: [String: String]
 	
+	public init(ids: [String: String]) {
+		self.ids = ids
+	}
+	
+	/* If two or more tagged ID have the same tag, the last ID will win. */
+	public init(taggedIDs: [TaggedID]) {
+		self.ids = Dictionary(taggedIDs.map{ ($0.tag, $0.id) }, uniquingKeysWith: { _, new in new })
+	}
+	
 }
 
 
